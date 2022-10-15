66deaeaa8be9772001ed872c88ecfed6e2b954ef
strangeal <bajerijustice@gmail.com>
2022-10-15 07:17:16 -0400

    Add message

--- src/index.js
+++ src/index.js
@@ -3,23 +3,29 @@
 import './style.css';
 import { getScore, addScore } from './modules/scoreBoard.js';
 import listPost from './modules/listPost.js';
+import {formMessage, showRefreshMsg} from './modules/formMessage.js'
+
 
 window.onload = async () => {
   listPost(await getScore());
 };
 const refreshBtn = document.querySelector('.refresh__btn');
 refreshBtn.addEventListener('click', async () => {
+  showRefreshMsg()
   const list = await getScore();
   listPost(list);
 });
 
+
 const inputForm = document.querySelector('.scores__form');
+
 inputForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const values = {
     user: document.querySelector('#input__name').value,
     score: document.querySelector('#input__score').value,
   };
+  formMessage()
   inputForm.reset();
-  await addScore(values);
+  await addScore(values);  
 });
